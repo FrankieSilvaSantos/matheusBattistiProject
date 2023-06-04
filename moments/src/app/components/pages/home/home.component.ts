2d6aab518aca10ba82452aaf60693bf76d668c66
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2023-06-04 20:33:34 -0300

    search

--- moments/src/app/components/pages/home/home.component.ts
+++ moments/src/app/components/pages/home/home.component.ts
@@ -14,6 +14,8 @@ export class HomeComponent {
   allMoments: Moment[] = []
   moments: Moment[] = []
   baseApiUrl = environment.baseApiUrl
+  faSearch = faSearch
+  searchTerm:string = ''
 
   constructor(private momentService: MomentService) {}
 
@@ -32,6 +34,28 @@ export class HomeComponent {
       this.allMoments = data
       this.moments = data
     })
+
+
   }
+
+  search(e:Event):void {
+
+     const target = e.target as HTMLInputElement
+     const value = target.value
+
+ 
+
+     this.moments = this.allMoments.filter((moment) => {
+       return  moment.title.toLowerCase().includes(value)
+       })
+    
+ 
+   
+  }
+  
+
+
+ 
+
 
 }
